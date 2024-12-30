27804790cc5307c36bcf5a4d9a24f5c89891d9f8
PrincePratihar <148641642+PrincePratihar@users.noreply.github.com>
2024-12-30 21:56:25 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,30 @@
+export const data = [
+	{
+		id: 1,
+		date: "December 2024",
+		title: "Tigra Dam,Gwalior",
+		description:"Tighra dam is located just outside the main city and provides a nice getaway from the city. The 1341 m long Tighra dam plays a significant role in supplying water to the entire city.",
+		coverImg: "3.jpg",
+		country: "India",
+		googleMapLink:"https://maps.app.goo.gl/SdkNRWMZtdAKv4gu9",
+	},
+	{
+		id: 2,
+		date: "October 2024",
+		title: "Madhav National Park,Shivpuri",
+		description:"Blessed with perennial carpeting of the conifers and the squeaky cleanliness in air, this park boasts of some of the most exquisite species like the little Chinkara, the Indian gazelle, and the Chital, Nilgai, Sambar, Chausingha or four-horned Antelope, Blackbuck, Sloth Bear, Leopard and the omnipresent common Langur. In terms of aves, this park is host to species like the white-breasted kingfisher, pond heron, purple-sunbird and painted stork.",
+		coverImg: "2.jpg",
+		country: "India",
+		googleMapLink:"https://maps.app.goo.gl/ULDM8S99qFLV2KdQ9"
+	},
+	{
+		id: 3,
+		date: "November 2024",
+		title: "Gwalior Fort, Gwalior",
+		description:"Referred to as 'the pearl amongst fortresses in India' by Mughal emperor Babur, the Gwalior Fort is one of the most impenetrable fortresses located in the entire northern and southern India and is a place you definitely must visit. Situated on the top of a vast rocky mountain near Gwalior in Madhya Pradesh in central India, this imposing structure dominates the entire city of Gwalior. An inseparable part of the identity and architecture of the city, evidence suggests that it has been around since 6th Century. It is also the places where the second oldest reference of the number 'zero' has been found in the form of a carving inside a temple on the top of the fort.",
+		coverImg: "1.jpg",
+		country: "India",
+		googleMapLink:"https://maps.app.goo.gl/uFQ9kMCkY6oGFtNg8"
+	}
+
+]
